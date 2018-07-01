2dadc59c021a6b3bc4d02ffcd5c688148dc41cd5
Sunil Kumar Singh <sunilsinghmbm@gmail.com>
2018-07-02 00:20:26 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+
+export const routeConfig: Routes = [
+  {
+      path: 'Home',
+      component: HomeComponent
+  },
+  {
+      path: 'Services',
+      component: ServicesComponent
+  },
+  {
+    path: 'Products',
+    component: ProductsComponent
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  },
+  {
+      path: '',
+      redirectTo: '/Home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**', 
+      component: PageNotFoundComponent,
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent,
+    HomeComponent,
+    ServicesComponent,
+    ProductsComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-navigation/top-navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-top-navigation',
+  templateUrl: './top-navigation.component.html',
+  styleUrls: ['./top-navigation.component.css']
+})
+export class TopNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
